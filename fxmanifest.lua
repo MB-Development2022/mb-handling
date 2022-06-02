f84e4f1a5dddba765611ebd8b2cadefdce3b8989
ItzKizar <99242342+ItzKizar@users.noreply.github.com>
2022-06-02 01:01:45 -0400

    Upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+version '1.0.2'
+author 'MB Development'
+description 'Custom vehicle handling for stock gta 5 vehicles'
+
+
+
+data_file 'HANDLING_FILE' 'data/handling.meta'
+
+
+
+files {
+    'data/handling.meta',
+}
